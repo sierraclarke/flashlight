32ad096d368c8f8f391178b70202d18042d5ebab
Sierra <sierra.clarke@brooklynsteamcenter.org>
2019-04-18 14:24:23 -0400

    Initial Commit

---
+++ flashlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flashlight
+//
+//  Created by Genesis on 4/18/19.
+//  Copyright © 2019 Genesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
